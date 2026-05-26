7be5337d06840a4118fc8126429e63b888c3d5b2
6631501021 <6631501021@lamduan.mfu.ac.th>
2026-05-26 20:30:37 +0700

    setup upload

--- src/views/RealtimeView.vue
+++ src/views/RealtimeView.vue
@@ -135,8 +135,8 @@ const showModal = ref(false)
 const newCamName = ref('')
 
 // 🚨 กำหนด Base URL ของ Backend (ถ้าใช้ Cloudflare tunnel ก็เปลี่ยนตรงนี้ที่เดียวจบ!)
-const BACKEND_URL = 'https://obituaries-advised-dispatched-latino.trycloudflare.com' 
-// const BACKEND_URL = 'https://seeds-processors-spots-morgan.trycloudflare.com'
+const BACKEND_URL = 'https://obituaries-advised-dispatched-latino.trycloudflare.com'  
+// const BACKEND_URL = 'https://seeds-processors-spots-morgan.trycloudflare.com '
 
 const cameras = ref([
   {
